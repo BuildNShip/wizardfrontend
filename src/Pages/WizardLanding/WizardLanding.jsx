081e316e52362b8296c4fe1110f1845c672d2afb
AswinAsok <aswinasokofficial@gmail.com>
2023-03-05 12:23:55 +0530

    master:basic-field-added

--- src/Pages/WizardLanding/WizardLanding.jsx
+++ src/Pages/WizardLanding/WizardLanding.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
-import styles from "./WizardLanding.module.css"
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputLeftAddon,
+  InputRightAddon,
+  Select,
+  Textarea,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+import styles from "./WizardLanding.module.css";
 
 const WizardLanding = () => {
   return (
-    <div>WizardLanding</div>
-  )
-}
+    <div className={styles.main_container}>
+      <div className={styles.first_view_container}>
+        <div className={styles.first_view}>
+          <Select placeholder="Select Type Request">
+            <option value="POST">POST</option>
+            <option value="GET">GET</option>
+          </Select>
+          <InputGroup size="sm">
+            <InputLeftAddon children="https://" />
+            <Input placeholder="mysite" />
+            <InputRightAddon children=".com" />
+          </InputGroup>
+          <Button colorScheme="teal" size="md">
+            Sent Request
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default WizardLanding
+export default WizardLanding;

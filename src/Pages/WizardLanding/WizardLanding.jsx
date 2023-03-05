be7b632935ec65313d19ac7cc34245ddc8e255f1
AswinAsok <aswinasokofficial@gmail.com>
2023-03-05 16:52:15 +0530

    dev:making-chages

--- src/Pages/WizardLanding/WizardLanding.jsx
+++ src/Pages/WizardLanding/WizardLanding.jsx
@@ -7,8 +7,6 @@ import {
   FormLabel,
   Input,
   InputGroup,
-  InputLeftAddon,
-  InputRightAddon,
   Select,
   Textarea,
   Table,
@@ -55,6 +53,12 @@ const WizardLanding = () => {
     }
   };
 
+  const convertArrayToObject = (array) =>
+    array.reduce((obj, item) => {
+      obj[item.key] = item.value;
+      return obj;
+    }, {});
+
   const [token, setToken] = useState("");
 
   const handleTokenChange = (event) => {
@@ -65,105 +69,145 @@ const WizardLanding = () => {
     <div className={styles.main_container}>
       <div className={styles.first_view_container}>
         <div className={styles.first_view}>
-          <div className={styles.row}>
-            <Select
-              onChange={(e) => {
-                setType(e.target.value);
-              }}
-              margin="1rem"
-              maxWidth="8rem"
-              placeholder="Select Type Request"
-            >
-              <option value="POST">POST</option>
-              <option value="GET">GET</option>
-            </Select>
-            <InputGroup margin="1rem" size="md">
-              <Input
-                onChange={() => {
-                  setLink(link);
+          <p className={styles.fv_heading}>The Wizard API</p>
+
+          <p className={styles.fv_tagline}>
+            Are you a frontend developer, trying to test your frontend project?.
+            But the api is not ready? Then this app is for you/
+          </p>
+          <div className={styles.row_container}>
+            <div className={styles.instruction_box}>
+              <p className={styles.ins_header}>Select Request & Request URL.</p>
+              <p className={styles.ins_tagline}>
+                Select the type in which you want the request and enter in the
+                URL where the site needs to be served
+              </p>
+            </div>
+            <div className={styles.row}>
+              <Select
+                onChange={(e) => {
+                  setType(e.target.value);
                 }}
-                placeholder="mysite"
-              />
-            </InputGroup>
+                margin="1rem"
+                maxWidth="8rem"
+                placeholder="Select Type Request"
+              >
+                <option value="POST">POST</option>
+                <option value="GET">GET</option>
+              </Select>
+              <InputGroup margin="1rem" size="md">
+                <Input
+                  onChange={() => {
+                    setLink(link);
+                  }}
+                  placeholder="mysite"
+                />
+              </InputGroup>
+            </div>
           </div>
-
-          <Box p={4}>
-            <FormControl id="token" isRequired>
-              <FormLabel>Token:</FormLabel>
-              <Input
-                type="text"
-                value={token}
-                onChange={handleTokenChange}
-                placeholder="Enter your token"
-              />
-            </FormControl>
-          </Box>
-
-          <Box>
-            <Table variant="simple">
-              <Thead>
-                <Tr>
-                  <Th>Key</Th>
-                  <Th>Value</Th>
-                </Tr>
-              </Thead>
-              <Tbody>
-                {parameters.map((param, index) => (
-                  <Tr key={index}>
-                    <Td>
-                      <FormControl isRequired>
-                        <Input
-                          value={param.key}
-                          onChange={(e) =>
-                            handleInputChange(index, "key", e.target.value)
-                          }
-                          onKeyDown={(e) => handleKeyDown(e, index)}
-                        />
-                      </FormControl>
-                    </Td>
-                    <Td>
-                      <FormControl isRequired>
-                        <Input
-                          value={param.value}
-                          onChange={(e) =>
-                            handleInputChange(index, "value", e.target.value)
-                          }
-                          onKeyDown={(e) => handleKeyDown(e, index)}
-                        />
-                      </FormControl>
-                    </Td>
+          <div className={styles.row_container}>
+            <div className={styles.instruction_box}>
+              <p className={styles.ins_header}>Enter Token</p>
+              <p className={styles.ins_tagline}>
+                Enter the parameters for the request. You can add as many as you
+                want. Press enter to add a new parameter.
+              </p>
+            </div>
+            <Box p={4}>
+              <FormControl id="token" isRequired>
+                <Input
+                  type="text"
+                  value={token}
+                  onChange={handleTokenChange}
+                  placeholder="Enter your token"
+                />
+              </FormControl>
+            </Box>
+          </div>
+          <div className={styles.row_container}>
+            <div className={styles.instruction_box}>
+              <p className={styles.ins_header}>Enter Parameters</p>
+              <p className={styles.ins_tagline}>
+                Enter the parameters for the request. You can add as many as you
+                want. Press enter to add a new parameter.
+              </p>
+            </div>
+            <Box>
+              <Table variant="simple">
+                <Thead>
+                  <Tr>
+                    <Th>Key</Th>
+                    <Th>Value</Th>
                   </Tr>
-                ))}
-              </Tbody>
-            </Table>
-          </Box>
-
-          <Box p={4}>
-            <Textarea
-              value={jsonData}
-              onChange={(e) => {
-                setJsonData(e.target.value);
-                setError(null);
-              }}
-              placeholder="Enter JSON data..."
-              size="lg"
-              resize="vertical"
-              height="300px"
-            />
-            {error && (
-              <Alert marginTop="1rem" status="error" variant="left-accent">
-                <AlertIcon />
-                {error}
-              </Alert>
-            )}
-          </Box>
+                </Thead>
+                <Tbody>
+                  {parameters.map((param, index) => (
+                    <Tr key={index}>
+                      <Td>
+                        <FormControl isRequired>
+                          <Input
+                            value={param.key}
+                            onChange={(e) =>
+                              handleInputChange(index, "key", e.target.value)
+                            }
+                            onKeyDown={(e) => handleKeyDown(e, index)}
+                          />
+                        </FormControl>
+                      </Td>
+                      <Td>
+                        <FormControl isRequired>
+                          <Input
+                            value={param.value}
+                            onChange={(e) =>
+                              handleInputChange(index, "value", e.target.value)
+                            }
+                            onKeyDown={(e) => handleKeyDown(e, index)}
+                          />
+                        </FormControl>
+                      </Td>
+                    </Tr>
+                  ))}
+                </Tbody>
+              </Table>
+            </Box>
+          </div>
+          <div className={styles.row_container}>
+            <div className={styles.instruction_box}>
+              <p className={styles.ins_header}>Enter Response(JSON)</p>
+              <p className={styles.ins_tagline}>
+                Enter in the format you want the api to return. You can use the
+                JSON format. This resposne will be returned when you fetch the
+                api.
+              </p>
+            </div>
+            <Box p={4}>
+              <Textarea
+                value={jsonData}
+                onChange={(e) => {
+                  setJsonData(e.target.value);
+                  setError(null);
+                }}
+                placeholder="Enter JSON data..."
+                size="lg"
+                resize="vertical"
+                height="300px"
+                width="20re"
+              />
+              {error && (
+                <Alert marginTop="1rem" status="error" variant="left-accent">
+                  <AlertIcon />
+                  {error}
+                </Alert>
+              )}
+            </Box>
+          </div>
 
           <Button
             onClick={() => {
               handleParseJson();
               console.log(type);
               console.log(link);
-              console.log(parameters);
+              console.log(convertArrayToObject(parameters));
               console.log(jsonData);
             }}
             margin="1rem"

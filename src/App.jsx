27f1c6b310186d1fb3cb736f46fb963ef3593c1e
AswinAsok <aswinasokofficial@gmail.com>
2023-03-05 11:32:11 +0530

    master:project-initalized

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import WizardLanding from "./Pages/WizardLanding/WizardLanding";
+
+function App() {
+  return (
+    <div className="App">
+      <WizardLanding />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/WizardLanding/WizardLanding.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from "./WizardLanding.module.css"
+
+const WizardLanding = () => {
+  return (
+    <div>WizardLanding</div>
+  )
+}
+
+export default WizardLanding
